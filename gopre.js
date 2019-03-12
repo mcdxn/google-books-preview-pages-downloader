a6d1988288fb725dcc14fa4eb54c2b937a1e15d9
Mark <markcodedesign@gmail.com>
2019-03-13 01:13:49 +0900

    Create gopre.js
    Initial commit

---
+++ gopre.js
@@ -0,0 +1,45 @@
+var gopre = (function (){
+    let book = document.getElementById("viewport");
+    let observer = null;
+    let links = [];
+    let targets = [];
+
+    let callback = function(mutationsList, observer) {
+        
+        for(let mutation of mutationsList) {
+            if (mutation.type == 'childList') {
+                
+                targets = mutation.target.getElementsByTagName("img");
+
+                if(targets) {
+                    for(let target of targets){
+                        links.push(target.src);
+                    }
+                }
+
+            }
+        }
+    }
+
+    
+    return { 
+        start: function(){
+            observer = new MutationObserver(callback);
+            observer.observe(book, { attributes: true, childList: true, subtree: true });        
+        },
+
+        finish: function(){
+        let uniqLinks = new Set(links);
+        let newWindow = window.open();
+        
+        for(let link of uniqLinks) {
+            newWindow.document.write('<br>');
+            newWindow.document.write(link);
+        }
+
+        observer.disconnect();
+        observer = null;
+    } 
+};
+
+})();
